a206d6d8a6e228af219770d28d7dff20a3945d31
hariurdino <163513013+hariurdino@users.noreply.github.com>
2025-07-12 14:05:56 +0700

    Update site.js

--- src/utils/site.js
+++ src/utils/site.js
@@ -6,4 +6,4 @@ export const terbit = '2025-02-17T00:03:13Z';
 export const google = '6qI659qtEQ7AF6zDOYeiKDNJWxoXIK8969hQllEfyJg';
 export const yandex = '6d07cbe75fc87980'
 
-export const index = '7d4d9fd7-36c0-4220-b9d8-ca79325756da';
+export const index = '83d12be4-1f26-4e02-9351-d944ec225dea';
